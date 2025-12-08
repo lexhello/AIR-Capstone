85a4961cb68c5fa88e867703a928da402b576565
Alexa L <alexa.lowe@gmail.com>
2025-12-07 19:31:00 -0500

    new wav files

--- generate_new_notes.py
+++ generate_new_notes.py
@@ -2,7 +2,8 @@ import librosa
 import soundfile as sf
 
 def pitch_shift_note(input_file, output_file, semitones):
-    audio, sr = librosa.load(input_file, sr=None)
+    high_sr = 48000
+    audio, sr = librosa.load(input_file, sr=high_sr)
 
     shifted = librosa.effects.pitch_shift(
         y=audio,
@@ -13,7 +14,7 @@ def pitch_shift_note(input_file, output_file, semitones):
     sf.write(output_file, shifted, sr)
     print("Saved:", output_file)
 
-input_file = "static/audio/guitar-c4.wav"
+input_file = "static/audio/guitar-c4-old.wav"
 
-pitch_shift_note(input_file, "static/audio/guitar-d5.wav", 14)
-pitch_shift_note(input_file, "static/audio/guitar-e5.wav", 16)
+pitch_shift_note(input_file, "static/audio/guitar-d5.wav", 2)
+pitch_shift_note(input_file, "static/audio/guitar-e5.wav", 4)
